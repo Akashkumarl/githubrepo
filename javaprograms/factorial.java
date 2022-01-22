8271adf2aba8dfceb68c8730617eed897308a0fe
Akashkumarl <96628545+Akashkumarl@users.noreply.github.com>
2022-01-22 23:58:31 +0530

    Create factorial.java

---
+++ javaprograms/factorial.java
@@ -0,0 +1,20 @@
+class factorial
+{
+  public static void main(String args[])
+  {
+    int num,i,j;
+    Scanner obj = new Scanner(System.in);
+    System.out.println("enter the factorial number");
+    num = obj.nextInt();
+    for(i=1;i<=num;i++)
+    { 
+      int fact=1;
+      for(j=1;j<=i;j++)
+      { 
+        fact*=j;
+      }
+      System.out.println("Factorial of "+i+"is"+fact);
+    }
+  }
+} 
+      

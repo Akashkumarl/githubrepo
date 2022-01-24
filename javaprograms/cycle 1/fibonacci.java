e3fce067c064fa919274e1b3e90176386d8617fd
Akashkumarl <96628545+Akashkumarl@users.noreply.github.com>
2022-01-24 18:55:30 +0530

    Create fibonacci.java

---
+++ javaprograms/cycle 1/fibonacci.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class fibonacci
+{
+  public static void main(String args[])
+  {
+    int first=0;second=1,third;
+    int n;
+    Scanner obj = new Scanner(System.in);
+    n = obj.nextInt();
+    System.out.println("The fibonacci series:"+first+" "+second);
+    for(int i=2;i<=n;i++)
+    {
+       third = first+second;
+       System.out.print(" "+third+" ");
+       first=second;
+       second=third;
+    }
+  }
+} 

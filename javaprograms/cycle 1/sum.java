1546871b81025d08eb8ca871e65544106e76e13b
Akashkumarl <96628545+Akashkumarl@users.noreply.github.com>
2022-01-24 18:58:50 +0530

    Create sum.java

---
+++ javaprograms/cycle 1/sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class sum
+{
+ public static void main(String args[])
+  {
+    int a,b,sum=0;
+    Scanner obj = new Scanner(System.in);
+    a=obj.nextInt();
+    b=obj.nextInt();
+    sum=a+b;
+    System.out.println("Result is"+sum);
+  }
+}  
+© 2022 GitHub, Inc.

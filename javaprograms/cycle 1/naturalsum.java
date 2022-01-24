8a3e2ef677399cc950200d2e5c681a4de659546f
Akashkumarl <96628545+Akashkumarl@users.noreply.github.com>
2022-01-24 18:57:07 +0530

    Create naturalsum.java

---
+++ javaprograms/cycle 1/naturalsum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class naturalsum
+{
+ public static void main(String args[])
+  {
+    int n,sum=0;
+    System.out.println("Enter the number");
+    Scanner obj = new Scanner(System.in);
+    n= obj.nextInt();
+    for(int i=1;i<=n;i++)
+    {
+       sum+=i;
+    }
+   System.out.println("the sum of first"+n+"natural numbers is;"+sum);
+ }
+}  
+      

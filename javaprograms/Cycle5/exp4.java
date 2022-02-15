003c375ba80d1659603a5e81764a637c21764678
Akashkumarl <96628545+Akashkumarl@users.noreply.github.com>
2022-02-15 15:14:11 +0530

    Add files via upload

---
+++ javaprograms/Cycle5/exp4.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+class exp4
+{
+ public static void main(String args[])throws Exception
+ {
+  try
+ {
+  FileInputStream fin = new FileInputStream("e1.txt");
+  int ch;
+  String a;int sum=0;
+  System.out.println("The numbers in file are");
+  while((ch=fin.read())!=-1)
+ {
+  String s = Character.toString((char)ch);
+  StringTokenizer st = new  StringTokenizer(s);
+  while(st.hasMoreTokens())
+ {
+  a = st.nextToken();
+int b = Integer.parseInt(a);
+System.out.println(b);
+sum=sum+b;
+}
+}
+System.out.println("the sum is"+sum);
+}
+catch(Exception e){System.out.print(e);}
+}
+}
+  

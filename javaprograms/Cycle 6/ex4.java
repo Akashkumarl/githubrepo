5d340b98cd949e1c738f0b17673354103677a965
Akashkumarl <96628545+Akashkumarl@users.noreply.github.com>
2022-02-15 15:17:16 +0530

    Add files via upload

---
+++ javaprograms/Cycle 6/ex4.java
@@ -0,0 +1,24 @@
+import java.io.*;
+class ex4
+{
+ public static void main(String args[])
+{
+ try
+{
+ int b = 100;
+ if(b<500)
+ throw new balanceException();
+}
+catch(balanceException e)
+{
+System.out.println(e);
+}
+}
+}
+class balanceException extends Exception
+{
+ balanceException()
+ {
+  System.out.println("low balance");
+}
+}
